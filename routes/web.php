60ea77bd13a78ce87a59bae087596b5594de0b3f
alexisbompart <alexisbompart@gmail.com>
2026-03-13 20:42:34 -0400

    cambio  WEB

--- routes/web.php
+++ routes/web.php
@@ -8,32 +8,34 @@ use App\Http\Controllers\ProductosController;
 use App\Http\Controllers\OrdenesController;
 use App\Http\Controllers\UsersController;
 
-Route::get('/', function () {
-    return redirect()->route('login');
-});
-
-Route::get('/login', [AuthController::class, 'showLoginForm'])->name('login');
-Route::post('/login', [AuthController::class, 'login']);
-Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
-
-Route::middleware(['auth'])->group(function () {
-    Route::middleware(['role:admin'])->group(function () {
-        Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
+Route::prefix('app')->group(function () {
+    Route::get('/', function () {
+        return redirect()->route('login');
     });
 
-    Route::middleware(['role:admin,operador,consultor'])->group(function () {
-        Route::resource('beneficiarios', BeneficiariosController::class);
-    });
+    Route::get('/login', [AuthController::class, 'showLoginForm'])->name('login');
+    Route::post('/login', [AuthController::class, 'login']);
+    Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
 
-    Route::middleware(['role:admin,operador'])->group(function () {
-        Route::resource('productos', ProductosController::class);
-        Route::get('/ordenes', [OrdenesController::class, 'index'])->name('ordenes.index');
-        Route::get('/ordenes/create/{tipo}', [OrdenesController::class, 'create'])->name('ordenes.create');
-        Route::post('/ordenes/{tipo}', [OrdenesController::class, 'store'])->name('ordenes.store');
-        Route::get('/ordenes/{tipo}/{id}', [OrdenesController::class, 'show'])->name('ordenes.show');
-    });
+    Route::middleware(['auth'])->group(function () {
+        Route::middleware(['role:admin'])->group(function () {
+            Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
+        });
 
-    Route::middleware(['role:admin'])->group(function () {
-        Route::resource('users', UsersController::class);
+        Route::middleware(['role:admin,operador,consultor'])->group(function () {
+            Route::resource('beneficiarios', BeneficiariosController::class);
+        });
+
+        Route::middleware(['role:admin,operador'])->group(function () {
+            Route::resource('productos', ProductosController::class);
+            Route::get('/ordenes', [OrdenesController::class, 'index'])->name('ordenes.index');
+            Route::get('/ordenes/create/{tipo}', [OrdenesController::class, 'create'])->name('ordenes.create');
+            Route::post('/ordenes/{tipo}', [OrdenesController::class, 'store'])->name('ordenes.store');
+            Route::get('/ordenes/{tipo}/{id}', [OrdenesController::class, 'show'])->name('ordenes.show');
+        });
+
+        Route::middleware(['role:admin'])->group(function () {
+            Route::resource('users', UsersController::class);
+        });
     });
 });
